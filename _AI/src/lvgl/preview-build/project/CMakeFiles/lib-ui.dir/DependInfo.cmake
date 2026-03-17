122dd02afc8936f74dec3d43485d22e6dffcce17
Kevin <ngo.kevinT@gmail.com>
2026-03-17 01:48:08 -0700

    Attempt to build locally

---
+++ _AI/src/lvgl/preview-build/project/CMakeFiles/lib-ui.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/lved/project/lv_editor_runtime_stub.c" "project/CMakeFiles/lib-ui.dir/lv_editor_runtime_stub.c.o" "gcc" "project/CMakeFiles/lib-ui.dir/lv_editor_runtime_stub.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ _AI/src/lvgl/preview-build/project/CMakeFiles/lib-ui.dir/lv_editor_runtime_stub.c.o.d
@@ -0,0 +1,2 @@
+project/CMakeFiles/lib-ui.dir/lv_editor_runtime_stub.c.o: \
+  /lved/project/lv_editor_runtime_stub.c
